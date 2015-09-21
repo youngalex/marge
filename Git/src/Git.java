dfc7abae4f8a24c9e6afabbee9a0ec6a2dfa71fa
Alex-asus <youngalexha@gmail.com>
2015-09-22 02:14:53 +0800

    22222

--- Git/src/Git.java
+++ Git/src/Git.java
@@ -5,6 +5,10 @@ public class Git {
 		System.out.println("yahoo1");
 		System.out.println("gmail1");
 		System.out.println("gmail2");
+		System.out.println("gmail2");
+		System.out.println("gmail2");
+	}
+	}
 	}
 
 }
